7da69a56436ec5a9cc88632a28a2f50ca4f0a87b
Nicolas Danelon <nicolas.danelon@softvision.com>
2020-11-28 15:43:24 -0300

    Adding react ⚛

---
+++ src/Components/Footer/index.js
@@ -0,0 +1,25 @@
+import './styles.css';
+
+const Footer = () => (
+  <div className="footer">
+    <p className="left">
+      Made by
+      <a href="https://www.linkedin.com/in/nicolasdanelon/"
+         target="_blank"
+         rel="noreferrer"
+      >
+        Nicolas Danelon
+      </a>
+    </p>
+    <p className="right">
+      <a href="https://github.com/nicolasdanelon/nicolasdanelon.github.io/"
+         target="_blank"
+         rel="noreferrer"
+      >
+        View the source code
+      </a>
+    </p>
+  </div>
+)
+
+export default Footer;
---
+++ src/Components/Home/index.js
@@ -0,0 +1,15 @@
+import './styles.css';
+import Header from '../Header';
+import Footer from '../Footer';
+import React from 'react';
+
+const Index = () => {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  )
+}
+
+export default Index;
